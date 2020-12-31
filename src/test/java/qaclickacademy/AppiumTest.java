49b7e755573c8249e8d1a61a23a80d0225683699
manisha <isha.neharkar@gmail.com>
2020-12-31 23:02:09 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAndriodApp()
+	{
+		System.out.println("NativeAndriodApp");
+		
+	}
+	
+	@Test
+	public void iosAPP()
+	{
+		System.out.println("iosAPP");
+	}
+}
